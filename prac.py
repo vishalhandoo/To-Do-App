7be1eba474c0c16751e4c4e284c7a804d0341e49
vishalhandoo <vishalhandoo067@gmail.com>
2024-06-06 13:28:05 +0530

    Implemented Web app for the To-Do app using Streamlit

--- prac.py
+++ prac.py
@@ -1,5 +1,8 @@
+
 import FreeSimpleGUI as sg
 from Convert import convert
+
+sg.theme("Black")
 
 feet_label = sg.Text("Enter feet: ")
 feet_input = sg.Input(key="feet")
@@ -9,14 +12,20 @@ inches_input = sg.Input(key="inches")
 
 button = sg.Button("Convert")
 output_label = sg.Text("", key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Convertor",
                    layout=[[feet_label, feet_input],
                            [inches_label, inches_input],
-                           [button, output_label]])
+                           [button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
     feet = float(values["feet"])
     inches = float(values["inches"])
 
